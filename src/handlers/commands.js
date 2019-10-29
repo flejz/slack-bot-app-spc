f3a1d5e7dd178e6714d804b3f946ce4f0ac57714
Jaime Flores <jaimelopesflores@gmail.com>
2019-10-29 15:50:57 +0100

    fetching weather info

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,3 +1,6 @@
+const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager();
+const axios = require('axios')
 const { parse } = require('url');
 
 // the main handler
@@ -9,14 +12,50 @@ exports.handler = async (event) => {
     return { statusCode: 200 };
   }
 
+  // retreiving the secret from secrets manager
+  const secretData = await secretsManager.getSecretValue({SecretId: process.env.SECRET}).promise();
+  const secretString = JSON.parse(secretData.SecretString);
+
+  /*
+  "token=y9JDN4wpJQEy72az28clPHgZ&team_id=TPZ8RC2FQ&team_domain=flejz-playground&channel_id=DPWK0NHD2&channel_name=directmessage&user_id=UPKJ5830T&user_name=jaimelopesflores&command=%2Fhey&text=ho&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTPZ8RC2FQ%2F801789999939%2FvL9qG9f9jiQzBkkXTXppWTxs&trigger_id=812796215268.815297410534.93e42dcf487804f27b0087dc576b65ca"
+  [
+    'token=y9JDN4wpJQEy72az28clPHgZ',
+    'team_id=TPZ8RC2FQ',
+    'team_domain=flejz-playground',
+    'channel_id=DPWK0NHD2',
+    'channel_name=directmessage',
+    'user_id=UPKJ5830T',
+    'user_name=jaimelopesflores',
+    'command=/hey',
+    'text=ho',
+    'response_url=https://hooks.slack.com/commands/TPZ8RC2FQ/801789999939/vL9qG9f9jiQzBkkXTXppWTxs',
+    'trigger_id=812796215268.815297410534.93e42dcf487804f27b0087dc576b65ca',
+  ]
+  */
+
   // format the key values
   const keyValues = event.body.split('&').map(item => item.split('='))
+  const find = (compare) => keyValues.find(([key]) => key === compare)
   console.log(JSON.stringify(keyValues));
 
+  // fetch weather information
+  const [, city] = find('text')
+  console.log(`city ${city}`);
+  const { data } = await axios.get('https://samples.openweathermap.org/data/2.5/find', {
+    params: {
+      q: city,
+      units: 'metric',
+      appid: secretString.Weather_Appid
+    }
+  });
+  console.log(`data ${JSON.stringify(data)}`);
+
   // get the response_url key-value from the body request
-  const [key, url] = keyValues.find(([key]) => key === 'response_url')
+  const [, url] = find('response_url')
   console.log(`url ${url}`);
   return await postToSlack(decodeURIComponent(url), `Hey-Ho, let's go!`);
+
+
 };
 
 const postToSlack = async (url, text) => {
