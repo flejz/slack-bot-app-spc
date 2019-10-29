b1215e5a36967ce46603ee0dea6d3235d8dff2d3
Jaime Flores <jaimelopesflores@gmail.com>
2019-10-29 15:09:16 +0100

    having standarized code

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,25 +1,22 @@
-/*
-    Licensed under MIT-0
-    This function processes events from Slack (received through API Gateway) and echoes them back to Slack.
-*/
-const AWS = require('aws-sdk');
 const { parse } = require('url');
 
-// The main handler
+// the main handler
 exports.handler = async (event) => {
-    console.log(JSON.stringify(event));
+  console.log(JSON.stringify(event));
 
-    if (event == undefined || !event.hasOwnProperty('body')) {
-        console.error('Malformed event');
-        return { statusCode: 200 };
-    }
+  if (event == undefined || !event.hasOwnProperty('body')) {
+    console.error('Malformed event');
+    return { statusCode: 200 };
+  }
 
-    const keyValues = event.body.split('&').map(item => item.split('='))
-    console.log(JSON.stringify(keyValues));
+  // format the key values
+  const keyValues = event.body.split('&').map(item => item.split('='))
+  console.log(JSON.stringify(keyValues));
 
-    const [key, url] = keyValues.find(([key]) => key === 'response_url')
-    console.log(`url ${url}`);
-    return await postToSlack(decodeURIComponent(url), `Hey-Ho, let's go!`);
+  // get the response_url key-value from the body request
+  const [key, url] = keyValues.find(([key]) => key === 'response_url')
+  console.log(`url ${url}`);
+  return await postToSlack(decodeURIComponent(url), `Hey-Ho, let's go!`);
 };
 
 const postToSlack = async (url, text) => {
@@ -29,56 +26,27 @@ const postToSlack = async (url, text) => {
     const data = { text };
 
     const options = {
-        hostname,
-        port: 443,
-        path,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        }
+      hostname,
+      port: 443,
+      path,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      }
     };
 
     const req = https.request(options, (res) => {
-        console.log(`statusCode: ${res.statusCode}`);
-        res.on('data', (d) => {
-            process.stdout.write(d);
-        });
-
-        resolve({statusCode: 200});
+      console.log(`statusCode: ${res.statusCode}`);
+      res.on('data', (d) => { process.stdout.write(d) });
+      resolve({statusCode: 200});
     });
 
-    // If error
     req.on('error', (e) => {
-        console.error('Error: ', e);
-        reject({statusCode: 200});
+      console.error('Error: ', e);
+      reject({statusCode: 200});
     });
 
-    // Send the request
     req.write(JSON.stringify(data));
     req.end();
-  });
-};
-
-const verifySignature = async (slackTimestamp,slackBody,slackSignature,signingSecret) => {
-  return new Promise((resolve,reject) => {
-    const crypto = require('crypto');
-
-    // Verifying if the timestamp is within 5 minutes from the current time
-    const currentTime = Math.floor(new Date().getTime()/1000);
-    if (Math.abs(currentTime - slackTimestamp) > 300) {
-        console.error('The request is older than 5 minutes');
-        resolve(false);
-    } else {
-        // Calculating and verifying the signature with headers in the Slack request
-        const sigBasestring = `v0:${slackTimestamp}:${slackBody}`;
-        const calculatedSignature = 'v0=' + crypto.createHmac('sha256', signingSecret).update(sigBasestring,'utf8').digest('hex');
-        if (calculatedSignature === slackSignature) {
-            resolve(true);
-        } else {
-            console.error('Signature does not match');
-            resolve(false);
-        }
-    }
-
   });
 };
--- src/handlers/events.js
+++ src/handlers/events.js
@@ -1,53 +1,49 @@
-/*
-    Licensed under MIT-0
-    This function processes events from Slack (received through API Gateway) and echoes them back to Slack.
-*/
 const AWS = require('aws-sdk');
 const secretsManager = new AWS.SecretsManager();
 
-// The main handler
+// the main handler
 exports.handler = async (event) => {
-    console.log(JSON.stringify(event));
+  console.log(JSON.stringify(event));
 
-    if (event == undefined || !event.hasOwnProperty('body')) {
-        console.error('Malformed event');
-        return { statusCode: 200 };
-    }
+  if (event == undefined || !event.hasOwnProperty('body')) {
+    console.error('Malformed event');
+    return { statusCode: 200 };
+  }
 
-    const eventBody = JSON.parse(event.body);
+  const eventBody = JSON.parse(event.body);
 
-    // Retreiving the secret from Secrets Manager
-    const secretData = await secretsManager.getSecretValue({SecretId: process.env.SECRET}).promise();
-    const secretString = JSON.parse(secretData.SecretString);
+  // retreiving the secret from secrets manager
+  const secretData = await secretsManager.getSecretValue({SecretId: process.env.SECRET}).promise();
+  const secretString = JSON.parse(secretData.SecretString);
 
-    // Verifying the signature to confirm that the message was sent by Slack
-    // Reference: https://api.slack.com/docs/verifying-requests-from-slack
-    const signatureVerified = await verifySignature (event.headers['X-Slack-Request-Timestamp'],
-                                                    event.body,
-                                                    event.headers['X-Slack-Signature'],
-                                                    secretString.Signing_Secret);
+  // verifying the signature to confirm that the message was sent by slack
+  // reference: https://api.slack.com/docs/verifying-requests-from-slack
+  const signatureVerified = await verifySignature (event.headers['X-Slack-Request-Timestamp'],
+    event.body,
+    event.headers['X-Slack-Signature'],
+    secretString.Signing_Secret);
 
-    if (!signatureVerified) {
-        console.error('Signature verification failed. Exiting.');
-        return {statusCode: 200};
-    } else
-        console.log('Signature verification succeeded.');
+  if (!signatureVerified) {
+    console.error('Signature verification failed. Exiting.');
+    return {statusCode: 200};
+  } else
+    console.log('Signature verification succeeded.');
 
-    // Challenge verification. If 'challenge' is present in the request, return the challenge value
-    if (eventBody.hasOwnProperty('challenge')) {
-        console.log('Challenge present in the request. Returning the challenge string back to Slack. Exiting.');
-        return { statusCode: 200, body: eventBody.challenge };
-    }
+  // challenge verification. if 'challenge' is present in the request, return the challenge value
+  if (eventBody.hasOwnProperty('challenge')) {
+    console.log('Challenge present in the request. Returning the challenge string back to Slack. Exiting.');
+    return { statusCode: 200, body: eventBody.challenge };
+  }
 
-    // In direct message conversations with bots, Slack sends bot's messages back to the events
-    // endpoint. If the subtype of the event is 'bot_message', the function will ignore it.
-    if (eventBody.event.subtype == 'bot_message') {
-        console.log("subtype = bot_message. Exiting.");
-        return { statusCode: 200 };
-    }
+  // in direct message conversations with bots, slack sends bot's messages back to the events
+  // endpoint. if the subtype of the event is 'bot_message', the function will ignore it.
+  if (eventBody.event.subtype == 'bot_message') {
+    console.log("subtype = bot_message. Exiting.");
+    return { statusCode: 200 };
+  }
 
-    console.log(`user ${eventBody.event.user} sent in channel ${eventBody.event.channel}, message ${eventBody.event.text}, of type ${eventBody.event.type} and subtype ${eventBody.event.subtype}`);
-    return await postToSlack(secretString.Bot_Token, eventBody.event.channel, `You said: ${eventBody.event.text}`);
+  console.log(`user ${eventBody.event.user} sent in channel ${eventBody.event.channel}, message ${eventBody.event.text}, of type ${eventBody.event.type} and subtype ${eventBody.event.subtype}`);
+  return await postToSlack(secretString.Bot_Token, eventBody.event.channel, `You said: ${eventBody.event.text}`);
 };
 
 const postToSlack = async (token, channel, text) => {
@@ -56,32 +52,27 @@ const postToSlack = async (token, channel, text) => {
     const data = { token, channel, text };
 
     const options = {
-        hostname: 'slack.com',
-        port: 443,
-        path: '/api/chat.postMessage',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Authorization': `Bearer ${token}`
-        }
+      hostname: 'slack.com',
+      port: 443,
+      path: '/api/chat.postMessage',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': `Bearer ${token}`
+      }
     };
 
     const req = https.request(options, (res) => {
-        console.log(`statusCode: ${res.statusCode}`);
-        res.on('data', (d) => {
-            process.stdout.write(d);
-        });
-
-        resolve({statusCode: 200});
+      console.log(`statusCode: ${res.statusCode}`);
+      res.on('data', (d) => { process.stdout.write(d) });
+      resolve({statusCode: 200});
     });
 
-    // If error
     req.on('error', (e) => {
-        console.error('Error: ', e);
-        reject({statusCode: 200});
+      console.error('Error: ', e);
+      reject({statusCode: 200});
     });
 
-    // Send the request
     req.write(JSON.stringify(data));
     req.end();
   });
